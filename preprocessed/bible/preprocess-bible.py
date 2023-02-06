68eecd6e0ac0d166288a5b5405f74a65c4ad7ea1
Jaime Fernandez Jr <jaimefernandez551@gmail.com>
2023-02-06 21:17:05 +0800

    add bible raw and  preprocessed

---
+++ preprocessed/bible/preprocess-bible.py
@@ -0,0 +1,33 @@
+import re
+import string
+
+def preprocess_text(text):
+    # lowercase text
+    text = text.lower()
+
+    # remove punctuation, excluding hyphens and apostrophes
+    exclude = re.sub(r'[-\'\s]', '', string.punctuation)
+    text = re.sub("[" + exclude + "]", '', text)
+
+    # remove numbers
+    text = re.sub(r'\d+', '', text)
+
+    # remove multiple whitespaces
+    text = re.sub(r'\s+', ' ', text)
+
+    return text
+
+def write_to_file(text, filename):
+    with open(filename, 'w') as f:
+        f.write(text)
+
+if __name__ == '__main__':
+    # Read the input text from file
+    with open('bible-raw.txt', 'r') as f:
+        text = f.read()
+
+    # Preprocess the text
+    preprocessed_text = preprocess_text(text)
+
+    # Write the preprocessed text to file
+    write_to_file(preprocessed_text, 'preprocessed_text.txt')
